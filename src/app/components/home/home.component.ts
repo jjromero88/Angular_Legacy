f5fe0a98ae5b80496733fa0ffb05f8b1d65b4395
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-09-20 20:29:36 -0500

    update

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit,
+         AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+
+// tslint:disable-next-line: no-conflicting-lifecycle
+@Component({
+  selector: 'app-home',
+  template: `
+
+    <app-ng-style></app-ng-style>
+    <br>
+
+    <app-css></app-css>
+    <br>
+
+    <app-clases></app-clases>
+    <br>
+
+    <p [appResaltado]="'orange'">
+        Hola Mundo
+    </p>
+
+    <br>
+    <app-ng-switch></app-ng-switch>
+
+  `,
+  styles: [
+  ]
+})
+
+// tslint:disable-next-line: max-line-length
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+}
